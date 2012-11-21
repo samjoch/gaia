ae8a86168a72dc1a9ad4872735ef29a58c942bac
Samuel Jochimek <samuel@guiora.com>
2012-11-21 14:56:24 +0100

    [Bug 811745] - polish wallpaper picker

--- apps/wallpaper/js/pick.js
+++ apps/wallpaper/js/pick.js
@@ -1,5 +1,51 @@
+var Wallpaper = {
+
+    thumbnail: { width: (window.innerWidth-2)/3, height: 159 }
+
+  , crop: function wallpapers_crop(el){
+
+      // Ratio of el (suppose to be an image element)
+      var r = el.width / el.height;
+
+      // Compute both possibilies
+      var w1 = this.thumbnail.width
+      ,   h1 = this.thumbnail.width / r
+      ,   w2 = r * this.thumbnail.height
+      ,   h2 = this.thumbnail.height;
+
+      // Set el dimension
+      if ( w2 > this.thumbnail.width ) {
+        el.width = Math.floor(w2);
+        el.height = Math.floor(h2);
+      } else {
+        el.width = Math.floor(w1);
+        el.height = Math.floor(h1);
+      }
+
+      // Set el position
+      var left = Math.floor((this.thumbnail.width - el.width) / 2)
+      ,   top = Math.floor((this.thumbnail.height - el.height) / 2);
+
+      el.style.left = left + 'px';
+      el.style.top  = top + 'px';
+    }
+
+  , init: function wallpapers_init(){
+      this.wallpapers = document.querySelectorAll('.wallpaper');
+      for(var i = 0; i < this.wallpapers.length; i++){
+        this.crop(this.wallpapers[i].firstChild);
+      }
+      var el = document.getElementById('wallpapers');
+      el && el.style.height = 'auto';
+    }
+
+};
+
 window.onload = function() {
-  navigator.mozSetMessageHandler('activity', function handler(activityRequest) {
+
+  Wallpaper.init();
+
+  navigator.mozSetMessageHandler && navigator.mozSetMessageHandler('activity', function handler(activityRequest) {
     var activityName = activityRequest.source.name;
     if (activityName !== 'pick')
       return;
@@ -20,6 +66,8 @@ window.onload = function() {
     // Ignore clicks that are not on one of the images
     if (!(e.target instanceof HTMLImageElement))
       return;
+
+    if(!pickActivity) { return; }
 
     var canvas = document.createElement('canvas');
     var context = canvas.getContext('2d');
@@ -47,4 +95,5 @@ window.onload = function() {
     cancelButton.removeEventListener('click', cancelPick);
     wallpapers.removeEventListener('click', pickWallpaper);
   }
+
 };
