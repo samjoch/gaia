d775ac602f687c4dd836a608063d22f431fd40c1
Samuel Jochimek <samuel@guiora.com>
2012-11-26 11:16:09 +0100

    Bug 811745 - update screen timeout select

--- apps/settings/js/screen_timeout.js
+++ apps/settings/js/screen_timeout.js
@@ -12,22 +12,17 @@ var ScreenTimeout = {
     this.el = document.getElementById('screen-timeout');
     this.select = this.el.querySelector('select');
     this.button = this.el.querySelector('button');
-    this.span   = this.el.querySelector('span');
   },
   
   update: function screen_timeout_update(){
-    this.span.innerHTML = this.select.options[this.select.selectedIndex].innerHTML;
+    this.button.innerHTML = this.select.options[this.select.selectedIndex].innerHTML;
   },
   
   bind: function screen_timeout_bind(){
     var self = this;
-    this.button.addEventListener('click', function screen_timeout_onClick(){
-      self.select.click();
-    });
     this.select.addEventListener('change', function screen_timeout_onChange(){
       self.update();
     });
-
   }
 
 };
